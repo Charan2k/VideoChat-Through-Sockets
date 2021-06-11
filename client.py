13ad3d436ff09a1767aac0f4a6a6135ca8100c9c
Charan Vaddepally <charan316.cv@gmail.com>
2021-06-11 15:22:42 +0530

    removed unnecessary comments and code

--- client.py
+++ client.py
@@ -6,24 +6,17 @@ import pickle
 def connect():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(("" , 8485))
-    # connection = client_socket.makefile('wb')
 
     cam = cv2.VideoCapture(1)
-
-    img_counter = 0
-
-    # encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
 
     while True:
         while cam.isOpened():
             ret, frame = cam.read()
-            # result, frame = cv2.imencode('.jpg', frame, encode_param)
             data = pickle.dumps(frame, 0)
             size = len(data)
 
 
             client_socket.sendall(struct.pack(">L", size) + data)
-            img_counter += 1
             cv2.imshow('Client Side',frame)
             if cv2.waitKey(1) == ord('q'):
                 exit(0)
